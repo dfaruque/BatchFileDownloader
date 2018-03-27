6e0c6e725e8c30e46a8965bea5b7ea686b5e39f3
dfaruque <dfaruque@gmail.com>
2018-03-27 12:17:13 +0600

    donloaded files

--- BatchFileDownloader/MainWindow.xaml.cs
+++ BatchFileDownloader/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,5 +26,58 @@ namespace BatchFileDownloader
         {
             InitializeComponent();
         }
+
+
+        private void btnDownload_Click(object sender, RoutedEventArgs e)
+        {
+
+            using (WebClient client = new WebClient())
+            {
+                client.DownloadFileAsync(new Uri("http://i.ebayimg.com/00/s/MTYwMFgxMjcw/z/4SIAAOSwM6NZsZ6u/$_12.JPG")
+                    , @"f:\image35.jpg");
+
+                client.DownloadFileCompleted += Client_DownloadFileCompleted;
+            }
+        }
+
+        private void Client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            labelProgress.Content = "completed";
+        }
+
+        //private TaskCompletionSource<bool> _tcs;
+
+        //private async void DownloadForm_Shown(object sender, EventArgs e)
+        //{
+        //    WebClient client = new WebClient();
+        //    client.DownloadProgressChanged += client_DownloadProgressChanged;
+        //    client.DownloadFileCompleted += client_DownloadFileCompleted;
+        //    await startDownload(client);
+        //}
+
+        //void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        //{
+        //    progressBar.Value = e.ProgressPercentage;
+        //    labelProgress.Content = String.Format("Downloaded {0} of {1} bytes", e.BytesReceived, e.TotalBytesToReceive);
+        //}
+
+        //void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        //{
+        //    // whatever else you have in this event handler, and then...
+        //    _tcs.SetResult(true);
+        //}
+
+        //private async Task startDownload(WebClient client)
+        //{
+        //    //files contains all URL links
+        //    foreach (string str in files)
+        //    {
+        //        //location is variable where file will be stored
+        //        _tcs = new TaskCompletionSource<bool>();
+        //        client.DownloadFileAsync(new Uri(str), location);
+        //        await _tcs.Task;
+        //    }
+        //    _tcs = null;
+        //}
     }
 }
